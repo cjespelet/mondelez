ceb99b9e689c4e3e766b9a1514d75bbc44c2ce61
cjespelet <cjespelet@gmail.com>
2025-05-16 11:45:09 -0300

    mondelex

--- main-server/config/database.js
+++ main-server/config/database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'memory_game_user',
-  host: 'localhost',
-  database: 'memory_game',
-  password: 'memory_game_password',
+  user: 'memory_game_3tzf_user',
+  host: 'dpg-d0jjk8buibrs73d3kgg0-a',
+  database: 'memory_game_3tzf',
+  password: '1GwP5Klclmb7BmYSlzURxNCqgusW3IM8',
   port: 5432,
 });
 
