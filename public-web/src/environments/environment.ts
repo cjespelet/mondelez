68f4d37364d685894179834780a89b10fd3fee3a
cjespelet <cjespelet@gmail.com>
2026-01-07 16:35:39 -0300

    games

--- public-web/src/environments/environment.ts
+++ public-web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://mondelez.onrender.com/api' // Ajusta esta URL según tu configuración
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://mondelez.onrender.com/api' // Ajusta esta URL según tu configuración
+  // apiUrl: 'http://localhost:3000/api'
 }; 
